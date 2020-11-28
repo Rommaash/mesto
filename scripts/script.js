3a8931af8ce6f38b8f6f0c76f8c33400ea9dca7e
Roman Abelenzev <ab.ad@yandex.ru>
2020-11-28 23:15:45 +0300

    7 часов на поиск ошибки в ID, таков путь. 1 проверка

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,4 @@
-const initialCards = [
-	{
-		name: 'Архыз',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-	},
-	{
-		name: 'Челябинская область',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-	},
-	{
-		name: 'Иваново',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-	},
-	{
-		name: 'Камчатка',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-	},
-	{
-		name: 'Холмогорский район',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-	},
-	{
-		name: 'Байкал',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-	}
-];
+
 
 
 
@@ -34,44 +9,64 @@ const buttonAddList = document.querySelector('.profile__button-add')
 const buttonCloseList = document.querySelectorAll('.popup__close');
 const nameInput = document.querySelector('.profile__title');
 const jobInput = document.querySelector('.profile__subtitle');
+// инпуты профиля
 const addNameInput = document.querySelector('.popup__input_type_name');
 const addJobInput = document.querySelector('.popup__input_type_job');
+
+
 const formElement = document.querySelector(".popup__button");
 const popImageText = document.querySelector('.popup__img-text');
 const popupElement = document.querySelector('.popup__element');
 const elementContent = document.querySelector('#element').content;
 const elements = document.querySelector('.elements');
-
 const cardName = popupElement.querySelector('#cardname');
 const cardLink = popupElement.querySelector('#cardlink');
-
 const popupImage = document.querySelector('.popup_img');
 const hugeImg = document.querySelector('.popup__img-photo');
+const popupAll = document.querySelectorAll('.popup')
 
+//функция открытия попапа
+function openPopup(popup) {
+	popup.classList.add('popup_opened');
+	document.addEventListener('click', closePopupOverlay);
+	document.addEventListener('keydown', closePopupOnEscape);
+}
 
+//функция закрытия через оверлей
+function closePopupOverlay(evt) {
+	if (evt.target.classList.contains('popup')) {
+		closePopup(evt.target);
+	}
+}
+
+//функция закрытия черещ Escape
+function closePopupOnEscape(evt) {
+	if (evt.key === 'Escape') {
+		const shownPopup = Array.from(popupAll).find(element => element.classList.contains('popup_opened'));
+		closePopup(shownPopup);
+	}
+}
+
+//функция закрытия попапов
+function closePopup(popup) {
+	popup.classList.toggle('popup_opened');
+	document.removeEventListener('click', closePopupOverlay);
+	document.removeEventListener('keydown', closePopupOnEscape);
+}
 
 const handlePopupProfile = () => {
 	addNameInput.value = nameInput.textContent;
 	addJobInput.value = jobInput.textContent;
 };
 
-function openPopup(popup) {
-	popup.classList.add('popup_opened');
-}
-
-function closePopup(popup) {
-	popup.classList.remove('popup_opened');
-}
-
-
-
-
+//функция переноса данных в профиле
 function HandleProfileSubmit(evt) {
 	evt.preventDefault();
 	nameInput.textContent = addNameInput.value;
 	jobInput.textContent = addJobInput.value;
 	closePopup(popupProfile);
 }
+
 
 function submitElement(evt) {
 	const newCard = {
@@ -82,15 +77,15 @@ function submitElement(evt) {
 	elements.prepend(createCard(newCard));
 	evt.preventDefault();
 }
+
 const removeElement = (event) => {
 	event.target.closest('.element').remove();
 };
 
-
+//функция создания карточек 
 function createCard(element) {
 	const cardElement = elementContent.cloneNode(true);
 	const cardElementImage = cardElement.querySelector('.element__image');
-
 	cardElement.querySelector('.element__info-text').textContent = element.name;
 	cardElement.querySelector('.element__image').src = element.link;
 	cardElement.querySelector('.element__image').alt = element.name;
@@ -113,10 +108,12 @@ function createCard(element) {
 buttonEditList.addEventListener('click', () => {
 	handlePopupProfile();
 	openPopup(popupProfile);
+
 });
 
 buttonAddList.addEventListener('click', () => {
 	openPopup(popupElement);
+
 });
 
 formElement.addEventListener('click', HandleProfileSubmit)
